d60ac4129e9954824f2413f86edccc394aec9125
Jakob Dennryd <jakob.dennryd@utb.ecutbildning.se>
2023-01-08 13:31:33 +0100

    Old repo bugged out so I created a new one, transferring code

---
+++ ApiCall.cs
@@ -0,0 +1,32 @@
+﻿namespace Umbraco
+{
+    public class ApiCall
+    {
+        public string value { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+    }
+
+
+    public class ApiClient
+    {
+        public static async Task<ApiCall?> GetApiResponseAsync()
+        {
+            var client = new HttpClient();
+
+            try
+            {
+                var result = await client.GetFromJsonAsync<ApiCall>("https://api.chucknorris.io/jokes/random");
+                return result;
+            }
+           catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+            
+        } 
+    }
+}
